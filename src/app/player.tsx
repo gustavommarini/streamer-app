7e230de74c9e1c7c401316b9bc876639fe90c62d
gustavommarini <gustavommarini@hotmail.com>
2025-05-28 14:41:04 +0200

    fix: Update player screen.

--- src/app/player.tsx
+++ src/app/player.tsx
@@ -1,15 +1,8 @@
 import React from "react";
-import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { COLORS } from "@/constants/colors";
 import { MediaPlayer } from "@/components";
 
 const PlayerScreen = () => {
-  return (
-    <>
-      <MediaPlayer />
-    </>
-  );
+  return <MediaPlayer />;
 };
 
 export default PlayerScreen;
